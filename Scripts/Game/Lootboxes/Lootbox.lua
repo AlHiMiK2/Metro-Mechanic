8ec01ac0c52a2cd0cec4bbbf9824c0188ffdc82a
Romytrix <ponapiix@gmail.com>
2025-12-11 23:10:45 +0300

    lootbox temporatly update(unfinished)

--- Scripts/Game/Lootboxes/Lootbox.lua
+++ Scripts/Game/Lootboxes/Lootbox.lua
@@ -3,36 +3,162 @@ dofile("$CONTENT_DATA/Scripts/Game/Lootboxes/loot_table.lua")
 Lootbox = class( nil )
 
 function Lootbox.server_onCreate( self )
+	self.destroyed = false
 end
 
 function Lootbox.client_onInteract( self, character, state )
-	if character:isPlayer() then
-		if state == true then
-			self.network:sendToServer("sv_use", {character = character})
+	if self.data.Open_Action == "Use" and self.data.Open_Type == "Both" then
+		if character:isPlayer() then
+			if state == true then
+				self.network:sendToServer("sv_use", {character = character})
+			end
 		end
 	end
 end
 
 function Lootbox.sv_use(self, param)
+	self.destroyed = true
 	local player = param.character:getPlayer()
 	
 	local container = player:getInventory()
 	if sm.container.beginTransaction() then
 
 		for _, item in ipairs(loot_table[self.data.Lootbox_Type]) do
-			local amount = math.random(item.min, item.max)
-			if amount > 0 then
-				sm.container.collect(container, item.uuid, amount)
+			if item.uuid ~= nil then
+				local amount = math.random(item.min, item.max)
+				if amount > 0 then
+					sm.container.collect(container, item.uuid, amount)
+				end
 			end
 		end
 
 		if sm.container.endTransaction() then
-			sm.effect.playEffect( "Mechanic land", self.shape.worldPosition )
-			sm.effect.playEffect( "Mechanic jump", self.shape.worldPosition )
-			sm.effect.playEffect( "Drill - Debris", self.shape.worldPosition )
+			for _, effect in ipairs(loot_table[self.data.Lootbox_Type].effects) do
+				sm.effect.playEffect( effect, self.shape.worldPosition)
+			end
+
 			self.shape:destroyShape(0)
 		end
+		self.destroyed = false
 	end
-	
-	--self.shape:des
+end
+
+
+function Lootbox.sv_hit(self, param)
+	if not self.destroyed and sm.exists( self.shape ) then
+		self.destroyed = true
+
+		local lootList = {}
+		for _, item in ipairs(loot_table[self.data.Lootbox_Type]) do
+			if item.uuid ~= nil then
+				local amount = math.random(item.min, item.max)
+				if amount > 0 then
+					lootList[i] = { uuid = item.uuid, quantity = amount }
+				end
+			end
+		end
+
+		SpawnLoot( self.shape, lootList, self.shape.worldPosition + sm.vec3.new( 0, 0, 1.0 ) )
+		
+		for _, effect in ipairs(loot_table[self.data.Lootbox_Type].effects) do
+			sm.effect.playEffect( effect, self.shape.worldPosition)
+		end
+		
+		self.shape:destroyShape(0)
+	end
+end
+
+
+
+
+
+function WheatPlant.server_onProjectile( self, hitPos, hitTime, hitVelocity, _, attacker, damage, userData, hitNormal, projectileUuid )
+	if self.data.Open_Action == "Hit" and self.data.Open_Type == "Both" then
+		self:sv_hit()
+	end
+end
+
+function WheatPlant.server_onMelee( self, hitPos, attacker, damage, power, hitDirection )
+	if self.data.Open_Action == "Hit" and self.data.Open_Type == "Both" then
+		self:sv_hit()
+	end
+end
+
+function WheatPlant.server_onExplosion( self, center, destructionLevel )
+	if self.data.Open_Action == "Hit" and self.data.Open_Type == "Both" then
+		self:sv_hit()
+	end
+end
+
+
+--ingame
+
+function SpawnLoot( origin, lootList, worldPosition, ringAngle )
+
+	if worldPosition == nil then
+		if type( origin ) == "Shape" then
+			worldPosition = origin.worldPosition
+		elseif type( origin ) == "Player" or type( origin ) == "Unit" then
+			local character = origin:getCharacter()
+			if character then
+				worldPosition = character.worldPosition
+			end
+		elseif type( origin ) == "Harvestable" then
+			worldPosition = origin.worldPosition
+		end
+	end
+
+	ringAngle = ringAngle or math.pi / 18
+
+	if worldPosition then
+		for i = 1, #lootList do
+			local dir
+			local up
+			if type( origin ) == "Shape" then
+				dir = sm.vec3.new( 1.0, 0.0, 0.0 )
+				up = sm.vec3.new( 0, 1, 0 )
+			else
+				dir = sm.vec3.new( 0.0, 1.0, 0.0 )
+				up = sm.vec3.new( 0, 0, 1 )
+			end
+
+			local firstCircle = 6
+			local secondCircle = 13
+			local thirdCircle = 26
+
+			if i < 6 then
+				local divisions = ( firstCircle - ( firstCircle - math.min( #lootList, firstCircle - 1 ) ) )
+				dir = dir:rotate( i * 2 * math.pi / divisions, up )
+				local right = dir:cross( up )
+				dir = dir:rotate( math.pi / 2 - ringAngle, right )
+			elseif i < 13 then
+				local divisions = ( secondCircle - ( secondCircle - math.min( #lootList - firstCircle + 1, secondCircle - firstCircle ) ) )
+				dir = dir:rotate( i * 2 * math.pi / divisions, up )
+				local right = dir:cross( up )
+				dir = dir:rotate( math.pi / 2 - 2 * ringAngle, right )
+			elseif i < 26 then
+				local dvisions = ( thirdCircle - ( thirdCircle - math.min( #lootList - secondCircle + 1, thirdCircle - secondCircle ) ) )
+				dir = dir:rotate( i * 2 * math.pi / dvisions, up )
+				local right = dir:cross( up )
+				dir = dir:rotate( math.pi / 2 - 4 * ringAngle, right )
+			else
+				-- Out of predetermined room, place randomly
+				dir = dir:rotate( math.random() * 2 * math.pi, up )
+				local right = dir:cross( up )
+				dir = dir:rotate( math.pi / 2 - ringAngle - math.random() * ( 3 * ringAngle ), right )
+			end
+
+			local loot = lootList[i]
+			local params = { lootUid = loot.uuid, lootQuantity = loot.quantity or 1, epic = loot.epic }
+			local vel = dir * (4+math.random()*2)
+			local projectileUuid = loot.epic and projectile_epicloot or projectile_loot
+			if type( origin ) == "Shape" then
+				sm.projectile.shapeCustomProjectileAttack( params, projectileUuid, 0, sm.vec3.new( 0, 0, 0 ), vel, origin, 0 )
+			elseif type( origin ) == "Player" or type( origin ) == "Unit" then
+				sm.projectile.customProjectileAttack( params, projectileUuid, 0, worldPosition, vel, origin, worldPosition, worldPosition, 0 )
+			elseif type( origin ) == "Harvestable" then
+				sm.projectile.harvestableCustomProjectileAttack( params, projectileUuid, 0, worldPosition, vel, origin, 0 )
+			end
+		end
+	end
 end
--- Scripts/Game/Lootboxes/loot_table.lua
+++ Scripts/Game/Lootboxes/loot_table.lua
@@ -1,5 +1,12 @@
 loot_table = {
-	["Toolbox_Standart"] = { 
+    --Only Usables
+	["Usables_Example"] = {
+        effects = {"Loot - Pickup"},
+        {uuid = sm.uuid.new("3c5c8d8c-d3c2-4952-a059-fbc9a641f2ac"), min = 1, max = 1}, --drilltool
+    },
+    
+	["Toolbox_Standart"] = {
+        effects = {"Mechanic land", "Mechanic jump", "Drill - Debris"},
         {uuid = sm.uuid.new("c5e56da5-bc3f-4519-91c2-b307d36e15aa"), min = 1, max = 5}, --screw1
         {uuid = sm.uuid.new("edd445cc-c298-4ce3-9a58-745c1bee1bc7"), min = 1, max = 5}, --screw2
         {uuid = sm.uuid.new("4a1b886b-913e-4aad-b5b6-6e41b0db23a6"), min = -2, max = 2}, --bearing
@@ -9,7 +16,8 @@ loot_table = {
         {uuid = sm.uuid.new("c7322cd1-3158-41d9-b15a-eff2f2f8d9f7"), min = -4, max = 5}, --inkammo
         {uuid = sm.uuid.new("3a3280e4-03b6-4a4d-9e02-e348478213c9"), min = -3, max = 2} --glowstick
     },
-	["Toolbox_Depo_Tower"] = { 
+	["Toolbox_Depo_Tower"] = {
+        effects = {"Mechanic land", "Mechanic jump", "Drill - Debris"},
         {uuid = sm.uuid.new("8305013f-f609-4925-9024-301d128c3211"), min = 1, max = 1}, --contact broom 1x1
         {uuid = sm.uuid.new("4a1b886b-913e-4aad-b5b6-6e41b0db23a6"), min = 4, max = 4}, --bearing
         {uuid = sm.uuid.new("36335664-6e61-4d44-9876-54f9660a8565"), min = 3, max = 3}, --glue
@@ -19,5 +27,15 @@ loot_table = {
         {uuid = sm.uuid.new("0dba257b-b907-4919-baaf-2fefe19f4e24"), min = 13, max = 13}, --small pipe
         {uuid = sm.uuid.new("8024db09-9147-4fe3-a747-00bc30ab724b"), min = 6, max = 6}, --small pipe bend
         {uuid = sm.uuid.new("bbc5cc77-443d-4aa7-a175-ebdeb09c2df3"), min = 4, max = 4} --pipe
+    },
+    --Only Hitables
+	["Hitables_Example"] = {
+        effects = {"Haybot - Destroyed", "ToteBot - DestroyedParts"},
+        {uuid = sm.uuid.new("1f7ac0bb-ad45-4246-9817-59bdf7f7ab39"), min = 10, max = 50} -- scrap metal
+    },
+    --Both
+	["Both_Example"] = { 
+        effects = {"Stone - BreakChunk small", "Haypile - Destruction"},
+        {uuid = sm.uuid.new("1b927252-ee6d-420e-8bb4-ae42fcb43ba8"), min = 5, max = 8} -- bricks
     }
 }
